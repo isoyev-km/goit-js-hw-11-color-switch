0cf025b09e7bb83ad387a1d6b6876a68b4b3cd7b
Kamil <60015567+isoyev-km@users.noreply.github.com>
2020-11-10 17:04:11 +0200

    task 1 done

---
+++ index.js
@@ -0,0 +1,34 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}; //ИСПОЛЬЗУЙ ФУНКЦИЮ ДЛЯ ГЕНЕРАЦИИ СЛУЧАЙНОГО ЧИСЛА
+
+const refs = {
+  startButton: document.querySelector('button[data-action="start"]'),
+  stopButton: document.querySelector('button[data-action="stop"]'),
+};
+
+changeColorFn = () => {
+  const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  document.body.style.backgroundColor = color;
+};
+
+let interval = undefined;
+
+refs.startButton.addEventListener("click", () => {
+  interval = setInterval(() => changeColorFn(), 1000);
+  refs.startButton.disabled = true;
+});
+
+refs.stopButton.addEventListener("click", () => {
+  clearInterval(interval);
+  refs.startButton.disabled = false;
+});
